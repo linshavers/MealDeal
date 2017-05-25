6853f8f56c69208b74876ebec22c0323e835aea0
linshavers <linshavers@gmail.com>
2017-05-25 17:26:32 -0400

    Homepage Edits

--- webpage/src/App.js
+++ webpage/src/App.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 class App extends Component {
+  constructor(props) {
+      super (props);
+      this.state = ({
+        name: null, 
+        address: null, 
+        distance: null, 
+      });
+    }
+
+
   render() {
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="body">
+
+        {/*App Logo Button */}
+        <div className="App">
+          <button className="App-logo-button" onClick= "linkToHomepage()"> 
+            <img src={require('/Users/lindseyshavers/Documents/Launch/MealDeal/webpage/src/Ok.png')}
+            />
+          </button>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
+        {/*Homepage Links to Find/Upload Pages*/}
+        <div align="center" className="App-links">
+          <table>
+            <tr>
+              <td><button className="App-find-button" onClick= "linkToFindPage()">Find Deals</button></td>
+              <td><button className="App-upload-button" onClick= "linkToFindPage()">Upload Deals</button></td>
+            </tr>
+          </table>
+        </div>
+
+      </div> //end of body class
+
     );
   }
 }
