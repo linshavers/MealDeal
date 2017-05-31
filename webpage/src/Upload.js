3a252bba401031169830806aa1adede68881584d
Paul Lee <paullee@Pauls-MBP.lan>
2017-05-31 10:49:17 -0400

    Upload Page + Firebase

--- webpage/src/Upload.js
+++ webpage/src/Upload.js
@@ -1,24 +1,47 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
-
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
 
 
 class Upload extends Component {
-
     constructor(props) {
       super (props);
-      this.state = ({
-        name: null, 
-        address: null, 
-        distance: null, 
-      });
+      this.state = {
+        restaurantName: [], 
+        dealItem: [], 
+        dealPrice: [],
+        dealDescription: [],
+        dealList: [],
+      };
     }
+
+    handleChange(field, e) {
+        this.setState({
+            ...this.state,
+            [field]: e.target.value,
+        })
+    }
+
+    handleSubmit() {
+        
+    }
+
 
 
     render() {
         return(
-            <h1>Upload Page</h1>
+            <MuiThemeProvider>
+                <div className = 'uploadForm'>
+                    <TextField hintText = "Name of Restaurant" onChange = { (e) => this.handleChange('newRestaurantName', e)}/><br/>
+                    <TextField hintText = "Deal Item" onChange = { (e) => this.handleChange('newRestaurantName', e)}/><br/>
+                    <TextField hintText = "Price" onChange = { (e) => this.handleChange('newRestaurantName', e)}/><br/>
+                    <TextField hintText = "Deal Description" onChange = { (e) => this.handleChange('newRestaurantName', e)}/><br/>
+                    <FlatButton label = "Submit" onClick={() => this.handleSubmit()}/><br/>
+                </div>
+            </MuiThemeProvider>
         )
     }
 }
--- webpage/src/index.js
+++ webpage/src/index.js
@@ -6,6 +6,18 @@ import './index.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Find from './Find.js';
 import Upload from './Upload.js';
+import * as firebase from 'firebase';
+
+//configures webpage to firebase database
+var config = {
+    apiKey: "AIzaSyD22XZBmWcs8-E7nnh3bhzU87QOs7tILME",
+    authDomain: "mealdeal-62730.firebaseapp.com",
+    databaseURL: "https://mealdeal-62730.firebaseio.com",
+    projectId: "mealdeal-62730",
+    storageBucket: "mealdeal-62730.appspot.com",
+    messagingSenderId: "1040391253525"
+  };
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
