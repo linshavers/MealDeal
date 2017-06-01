3845521125359be881443b8aa215bb91df869b3d
Paul Lee <paullee@Pauls-MBP.lan>
2017-06-01 16:32:43 -0400

    Updated

--- src/Deal.js
+++ src/Deal.js
@@ -3,5 +3,12 @@ import * as firebase from 'firebase';
 
 
 class Deal extends Component {
-    
+    render() {
+        return(
+            <h1>hi</h1>
+
+        )
+    }
 }
+
+export default Deal;
